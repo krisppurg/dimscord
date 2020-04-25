bd7e107c54a4c3c670b49b33e71008be9abd2338
TheGreenCat <mineharderytps801@gmail.com>
2020-04-25 12:24:13 +0100

    Update the constant version.

--- dimscord/constants.nim
+++ dimscord/constants.nim
@@ -4,7 +4,7 @@ var restVer* = "7"
 var base* = "https://discordapp.com/api/v" & restVer & "/"
 const
     libName* = "Dimscord"
-    libVer* = "0.0.8"
+    libVer* = "0.1.0"
 
     cdnBase* = "https://cdn.discordapp.com/"
     cdnCustomEmojis* = cdnBase & "emojis/"

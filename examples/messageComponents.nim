cdfac99db2c44dfa9ce3ac265466dd5cb6175ed3
Jake Leahy <jake@leahy.dev>
2021-07-12 22:18:43 +1000

    Added message component example

---
+++ examples/messageComponents.nim
@@ -0,0 +1,28 @@
+import dimscord, asyncdispatch, strutils, sequtils, options, tables
+# let discord = newDiscordClient("<your bot token goes here>")
+let discord = newDiscordClient("NzQyMDEwNzY0MzAyMjIxMzM0.Xy_5YA.djAXDK-oWzYKA7d1fWpkfa5X5qU")
+
+proc messageCreate(s: Shard, m: Message) {.event(discord).} =
+    let content = m.content
+    if m.author.bot or not content.startsWith("$$"): return
+    var components: seq[MessageComponent]
+    case content.replace("$$", "").toLowerAscii():
+        of "button":
+            components = @[MessageComponent(
+                kind: ActionRow,
+                components: @[MessageComponent(
+                    kind: Button,
+                    label: some "Press me",
+                    style: some Primary,
+                    customID: some "foobar" # Used to identify it later
+                )]
+            )]
+    echo components.len
+    if components.len > 0:
+        discard await discord.api.sendMessage(m.channelID, "hello", components = some components)
+
+proc onReady(s: Shard, r: Ready) {.event(discord).} =
+    echo "Ready as: " & $r.user
+
+# Connect to Discord and run the bot.
+waitFor discord.startSession()

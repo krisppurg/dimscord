296108b59a2bfa9820259be73b5aa07100325224
Jake Leahy <jake@leahy.dev>
2023-07-14 23:22:51 +1000

    Update example to use helpers

--- examples/message_components.nim
+++ examples/message_components.nim
@@ -2,15 +2,43 @@ import dimscord, asyncdispatch, strutils, options
 const token {.strdefine.} = "<your bot token goes here or use -d:token=(yourtoken)>"
 let discord = newDiscordClient(token)
 
-proc interactionCreate(s: Shard, i: Interaction) {.event(discord).} =
-    var msg = ""
-    let data = i.data.get
-    # You
-    if data.custom_id == "slmColours":
-        msg = "You selected " & data.values[0]
-    elif data.custom_id == "btnClick":
-        msg = "You clicked the button"
+proc messageCreate(s: Shard, m: Message) {.event(discord).} =
+    let content = m.content
+    if m.author.bot or not content.startsWith("$$"): return
+    var row = newActionRow()
+    case content.replace("$$", "").toLowerAscii():
+    of "button":
+        row &= newButton(
+            label = "click me!",
+            idOrUrl = "someUniqueID",
+            emoji = Emoji(name: some "🔥")
+        )
+    of "menu":
+        row &= newSelectMenu("someUniqueID", @[
+            newMenuOption("Red", "red", emoji = Emoji(name: some "🔥")),
+            newMenuOption("Green", "green"),
+            newMenuOption("Blue", "blue")
+        ])
+    else:
+      # Message doesn't match, ignore
+      return
 
+    discard await discord.api.sendMessage(
+        m.channel_id,
+        "hello",
+        components = @[row]
+    )
+    let iResp = discord.waitForComponentUse("someUniqueID")
+    # Wait 10 seconds for user to use a component
+    if not await iResp.withTimeout(10 * 1000):
+      return
+    let
+      i = await iResp
+      data = i.data.unsafeGet()
+    # Change the message depending on what the user used
+    let msg = if data.componentType == SelectMenu: "You selected " & data.values[0]
+              else: "You pressed the button"
+    # Respond back to the interaction
     await discord.api.interactionResponseMessage(
         i.id, i.token,
         kind = irtChannelMessageWithSource,
@@ -18,30 +46,6 @@ proc interactionCreate(s: Shard, i: Interaction) {.event(discord).} =
             content: msg
         )
     )
-
-proc messageCreate(s: Shard, m: Message) {.event(discord).} =
-    let content = m.content
-    if m.author.bot or not content.startsWith("$$"): return
-    var row = newActionRow()
-    case content.replace("$$", "").toLowerAscii():
-        of "button":
-            row &= newButton(
-                label = "click me!",
-                idOrUrl = "btnClick",
-                emoji = Emoji(name: some "🔥")
-            )
-        of "menu":
-            row &= newSelectMenu("slmColours", @[
-                newMenuOption("Red", "red", emoji = Emoji(name: some "🔥")),
-                newMenuOption("Green", "green"),
-                newMenuOption("Blue", "blue")
-            ])
-    if row.len > 0:
-        discard await discord.api.sendMessage(
-            m.channel_id,
-            "hello",
-            components = @[row]
-        )
 
 proc onReady(s: Shard, r: Ready) {.event(discord).} =
     echo "Ready as: " & $r.user

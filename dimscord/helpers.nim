2773466473cfb4e60f1570624a8c0bdd9b5ba3b4
Jake Leahy <jake@leahy.dev>
2023-09-26 10:28:04 +1000

    Perform check that event is correctly named
    Properly retain line info for better autocomplete

--- dimscord/helpers.nim
+++ dimscord/helpers.nim
@@ -65,12 +65,19 @@ macro event*(discord: DiscordClient, fn: untyped): untyped =
         newEmptyNode(),
         body
     )
+    anonFn.copyLineInfo(fn)
 
     if pragmas.findChild(it.strVal == "async").kind == nnkNilLit:
         anonFn.addPragma ident("async")
-
-    quote:
-        `discord`.events.`eventName` = `anonFn`
+    # Check the event is valid, give proper error if it isn't
+    if eventName.strVal.nimIdentNormalize() notin dimscordEvents:
+        fmt"{eventName} is not a valid dimscord event".error(eventName)
+    # Manually create the assignment so the line info is kept
+    # discord.events.`eventName` = anonFn
+    result = nnkAsgn.newTree(
+        discord.newDotExpr(ident"events").newDotExpr(ident $eventName),
+        anonFn
+    )
 
 proc defaultAvatarUrl*(u: User): string =
     ## Returns the default avatar for a user.
@@ -497,37 +504,6 @@ proc add*(component: var MessageComponent, item: SelectMenuOption) =
     component.options &= item
 
 # Event Handlers
-
-const procsTable = macrocache.CacheTable"dimscord.handlerTypes"
-    ## Stores a mapping of EventName -> parameters for event
-    ## Note:: The shared parameter is removed from them
-
-# Build up the procsTable
-static:
-    # getTypleImpl returns `ref ObjSym` so we need to get the impl of ObjSym
-    let impl = Events.getTypeImpl()[0].getImpl()
-    for identDefs in impl[2][2]:
-        var typ = identDefs[^2]
-
-        # Desym all the parameters
-        var params = newSeq[NimNode]()
-        # Skip return type and shard parameter
-        for identDef in typ[0][2 .. ^1]:
-            var newDef = nnkIdentDefs.newTree()
-            for param in identDef[0 ..< ^2]:
-                newDef &= ident $param
-            newDef &= identDef[^2]
-            newDef &= identDef[^1]
-            params &= newDef
-
-        for field in identDefs[0 ..< ^2]:
-            # Not exported so just ignore it
-            if field.kind == nnkIdent: continue
-            # Remove the on_ prefix for some events
-            let origName = $field[1]
-            let name = if origName == "on_dispatch": "unknown"
-                       else: dup(origName, removePrefix("on_"))
-            procsTable[name] = newStmtList(params).copy()
 
 proc params(event: DispatchEvent): NimNode =
     ## Returns the proc type stored for an event

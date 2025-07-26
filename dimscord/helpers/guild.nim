dcc705471a2ced901b0195a5c3b0deb30c47e857
swrge <swrgingrage@gmail.com>
2025-07-26 07:08:39 +0200

    helpers/guild: pass reason argument to `editGuild` call

--- dimscord/helpers/guild.nim
+++ dimscord/helpers/guild.nim
@@ -8,11 +8,11 @@ template beginPrune*(g: Guild;
     ## Begins a guild prune.
     getClient.api.beginGuildPrune(g.id, days, include_roles, compute_prune_count)
 
-template getPruneCount*(g: Guild, days: int): Future[int] =
+template getPruneCount*(g: Guild; days: int): Future[int] =
     ## Gets the prune count.
     getClient.api.getGuildPruneCount(g.id, days)
 
-template editMFA*(g: Guild, lvl: MFALevel; reason = ""): Future[MFALevel] =
+template editMFA*(g: Guild; lvl: MFALevel; reason = ""): Future[MFALevel] =
     ## Modify Guild MFA Level, requiring guild ownership.
     getClient.api.editGuildMFALevel(g.id, lvl, reason)
 
@@ -20,7 +20,7 @@ template delete*(g: Guild): Future[void] =
     ## Deletes a guild. Requires guild ownership.
     getClient.api.deleteGuild(g.id)
 
-template edit*(g: Guild;  # TODO: template overloading bug
+template edit*(g: Guild;
         name, description, region, afk_channel_id, icon = none string;
         discovery_splash, owner_id, splash, banner = none string;
         system_channel_id, rules_channel_id = none string;
@@ -47,21 +47,21 @@ template edit*(g: Guild;  # TODO: template overloading bug
         verification_level, default_message_notifications,
         system_channel_flags,
         explicit_content_filter, afk_timeout,
-        features, premium_progress_bar_enabled
+        features, premium_progress_bar_enabled, reason
     )
 
 template getAuditLogs*(g: Guild;
-        user_id, before = "", action_type = -1;
+        user_id, before = ""; action_type = -1;
         limit: range[1..100] = 50
 ): Future[AuditLog] =
     ## Get guild audit logs. The maximum limit is 100.
     getClient.api.getGuildAuditLogs(g.id, user_id, before, action_type, limit)
 
-template deleteRole*(g: Guild, r: Role): Future[void] =
-   ## Deletes a guild role.
-   getClient.api.deleteGuildRole(g.id, r.id)
+template deleteRole*(g: Guild; r: Role): Future[void] =
+    ## Deletes a guild role.
+    getClient.api.deleteGuildRole(g.id, r.id)
 
-template editRole*(g: Guild, r: Role;
+template editRole*(g: Guild; r: Role;
         name = none string;
         icon, unicode_emoji = none string;
         permissions = none PermObj; color = none int;
@@ -81,12 +81,12 @@ template getInvites*(g: Guild): Future[seq[InviteMetadata]] =
     ## Gets guild invites.
     getClient.api.getGuildInvites(g.id)
 
-template getVanity*(g: Guild): Future[tuple[code: Option[string], uses: int]] =
+template getVanity*(g: Guild): Future[tuple[code: Option[string]; uses: int]] =
     ## Get the guild vanity url. Requires the MANAGE_GUILD permission.
     ## `code` will be null if a vanity url for the guild is not set.
     getClient.api.getGuildVanityUrl(g.id)
 
-template editMember*(g: Guild, m: Member;
+template editMember*(g: Guild; m: Member;
         nick, channel_id, communication_disabled_until = none string;
         roles = none seq[string];
         mute, deaf = none bool;
@@ -100,11 +100,11 @@ template editMember*(g: Guild, m: Member;
         roles, mute, deaf, reason
     )
 
-template removeMember*(g: Guild, m: Member, reason = ""): Future[void] =
+template removeMember*(g: Guild; m: Member; reason = ""): Future[void] =
     ## Removes a guild member.
     getClient.api.removeGuildMember(g.id, m.user.id, reason)
 
-template getBan*(g: Guild, m: Member | string): Future[GuildBan] =
+template getBan*(g: Guild; m: Member | string): Future[GuildBan] =
     ## Gets guild ban.
     getClient.api.getGuildBan(g.id, (when m is Member: m.user.id else: m))
 
@@ -112,19 +112,19 @@ template getBans*(g: Guild): Future[seq[GuildBan]] =
     ## Gets all the guild bans.
     getClient.api.getGuildBans(g.id)
 
-template ban*(g: Guild, m: Member, delete_msg_days: range[0..7] = 0;
+template ban*(g: Guild; m: Member; delete_msg_days: range[0..7] = 0;
         reason = ""): Future[void] =
     ## Creates a guild ban.
     getClient.api.createGuildBan(g.id, m.user.id, delete_msg_days, reason)
 
-template bulkBan*(g: Guild,
+template bulkBan*(g: Guild;
         user_ids: seq[string];
         delete_message_seconds = 0;
         reason = ""): Future[void] =
     ## Creates a guild bulk ban.
     getClient.api.bulkGuildBan(g.id, user_ids, delete_message_seconds, reason)
 
-template removeBan*(g: Guild, u: User | string, reason = ""): Future[void] =
+template removeBan*(g: Guild; u: User | string; reason = ""): Future[void] =
     ## Removes a guild ban.
     getClient.api.removeGuildBan(g.id, if u is User: u.id else: u, reason)
 
@@ -136,7 +136,7 @@ template getWebhooks*(g: Guild): Future[seq[Webhook]] =
     ## Gets a list of a channel's webhooks.
     getClient.api.getGuildWebhooks(g.id)
 
-template deleteIntegration*(integ: Integration, reason = ""): Future[void] =
+template deleteIntegration*(integ: Integration; reason = ""): Future[void] =
     ## Deletes a guild integration.
     getClient.api.deleteGuildIntegration(integ.id, reason)
 
@@ -144,12 +144,12 @@ template preview*(g: Guild): Future[GuildPreview] =
     ## Gets guild preview.
     getClient.api.getGuildPreview(g.id)
 
-template searchMembers*(g: Guild, query = "";
+template searchMembers*(g: Guild; query = "";
         limit: range[1..1000] = 1): Future[seq[Member]] =
     ## Search for guild members.
     getClient.api.searchGuildMembers(g.id, query, limit)
 
-template editEmoji*(g: Guild, e: Emoji, name = none string;
+template editEmoji*(g: Guild; e: Emoji; name = none string;
         roles = none seq[string];
         reason = ""
 ): Future[Emoji] =
@@ -157,7 +157,7 @@ template editEmoji*(g: Guild, e: Emoji, name = none string;
     assert e.id.isSome, "Cannot edit Emoji: the emoji might not be custom"
     getClient.api.editGuildEmoji(g.id, e.id.unsafeGet(), name, roles, reason)
 
-template deleteEmoji*(g: Guild, e: Emoji, reason = ""): Future[void] =
+template deleteEmoji*(g: Guild; e: Emoji; reason = ""): Future[void] =
     ## Deletes a guild emoji.
     assert e.id.isSome, "Cannot delete Emoji: the emoji might not be custom"
     getClient.api.deleteGuildEmoji(g.id, e.id.unsafeGet(), reason)
@@ -166,19 +166,20 @@ template getRegions*(g: Guild): Future[seq[VoiceRegion]] =
     ## Gets a guild's voice regions.
     getClient.api.getGuildVoiceRegions(g.id)
 
-template editSticker*(g: Guild, s: Sticker;
+template editSticker*(g: Guild; s: Sticker;
         name, desc, tags = none string;
         reason = ""
 ): Future[Sticker] =
     ## Modify a guild sticker.
     getClient.api.editGuildSticker(g.id, s.id, name, desc, tags, reason)
 
-template deleteSticker*(g: Guild, sk: Sticker, reason = ""): Future[Sticker] =
+template deleteSticker*(g: Guild; sk: Sticker; reason = ""): Future[Sticker] =
     ## Deletes a guild sticker.
-    getClient.api.deleteGuildSticker(sk.guild_id.get, sk.id, reason) # TODO: assert sk.guild_id.isSome, "Cannot delete Sticker: the bot is probably not in the sticker's owning guild."
+    getClient.api.deleteGuildSticker(sk.guild_id.get, sk.id,
+            reason) # TODO: assert sk.guild_id.isSome, "Cannot delete Sticker: the bot is probably not in the sticker's owning guild."
 
 template getScheduledEvent*(g: Guild;
-        event_id: string, with_user_count = false
+        event_id: string; with_user_count = false
 ): Future[GuildScheduledEvent] =
     ## Get a scheduled event in a guild.
     getClient.api.getScheduledEvent(g.id, event_id, with_user_count)
@@ -187,7 +188,7 @@ template getScheduledEvents*(g: Guild): Future[seq[GuildScheduledEvent]] =
     ## Get all scheduled events in a guild.
     getClient.api.getScheduledEvents(g.id)
 
-template editEvent*(g: Guild, gse: GuildScheduledEvent; # TODO: template overloading bug
+template editEvent*(g: Guild; gse: GuildScheduledEvent;
         name, start_time, image = none string;
         channel_id, end_time, desc = none string;
         privacy_level = none GuildScheduledEventPrivacyLevel;
@@ -207,42 +208,41 @@ template editEvent*(g: Guild, gse: GuildScheduledEvent; # TODO: template overloa
         reason
     )
 
-template delete*(gse: GuildScheduledEvent, reason = ""): Future[void] =
-   ## Delete a scheduled event in guild.
-   getClient.api.deleteScheduledEvent(gse.guild_id, gse.id, reason)
+template delete*(gse: GuildScheduledEvent; reason = ""): Future[void] =
+    ## Delete a scheduled event in guild.
+    getClient.api.deleteScheduledEvent(gse.guild_id, gse.id, reason)
 
 template getEventUsers*(gse: GuildScheduledEvent;
-        limit = 100, with_member = false;
+        limit = 100; with_member = false;
         before, after = ""
 ): Future[seq[GuildScheduledEventUser]] =
     ## Gets the users and/or members that were subscribed to the scheduled event.
     getClient.api.getScheduledEventUsers(
-        gse.guild_id, gse.id,
-        limit, with_member, before, after
+      gse.guild_id, gse.id, limit, with_member, before, after
     )
 
 template getRules*(g: Guild): Future[seq[AutoModerationRule]] =
     ## Get a Guild's current AutoMod Rules
     getClient.api.getAutoModerationRules(g.id)
 
-template getRule*(g: Guild, rule_id: string): Future[AutoModerationRule] =
+template getRule*(g: Guild; rule_id: string): Future[AutoModerationRule] =
     ## Get a Guild's specific AutoMod Rule
     getClient.api.getAutoModerationRule(g.id, rule_id)
 
-template deleteRule*(g: Guild, amr: AutoModerationRule): Future[void] =
+template deleteRule*(g: Guild; amr: AutoModerationRule): Future[void] =
     ## deletes automod rule
     getClient.api.deleteAutoModerationRule(g.id, amr.id)
 
-template editRule*(g: Guild, amr: AutoModerationRule;
-    event_type = none int, name = none string;
+template editRule*(g: Guild; amr: AutoModerationRule;
+    event_type = none int; name = none string;
     trigger_type = none ModerationTriggerType;
     trigger_metadata = none tuple[
-        keyword_filter: seq[string],
+        keyword_filter: seq[string];
         presets: seq[int]
     ];
     actions = none seq[ModerationAction]; enabled = none bool;
     exempt_roles, exempt_channels = none seq[string];
-    reason = ""
+    reason = "";
 ): Future[AutoModerationRule] =
     ## Edits an automod rule.
     ## `event_type` is gonna be 1 for SEND_MESSAGE
@@ -253,3 +253,4 @@ template editRule*(g: Guild, amr: AutoModerationRule;
         enabled, exempt_roles, exempt_channels,
         reason
     )
+
